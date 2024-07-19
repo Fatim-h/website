dacd7165ddf6a01844af61f0e44dfa9167a07ecf
Fatim-h <167449143+Fatim-h@users.noreply.github.com>
2024-07-19 11:43:17 +0500

    Add files via upload

---
+++ homepagestyle.js
@@ -0,0 +1,19 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
